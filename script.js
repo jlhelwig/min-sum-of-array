59cf97f2e0984288b2463e31dcbaffe87d3871c3
jlhelwig <jeff.helwig@gmail.com>
2018-04-14 19:24:14 -0500

    final

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 minSum([5,4,2,3])//22
-// minSum([12,6,10,26,3,24])//342
-// minSum([9,2,8,7,5,4,0,6])//77
+minSum([12,6,10,26,3,24])//342
+minSum([9,2,8,7,5,4,0,6])//74
 
 function minSum(arr) {
-  let sortArr=arr.sort(function(a,b){
-    return b-a;
-  })
-  while(sortArr){
-    
-  }
+  sum=0
+    let sortArr=arr.sort(function(a,b){
+      return b-a;
+    })
+    console.log(sortArr);
+      for (let i=0; i<sortArr.length/2; i++){
+        sum=sum+sortArr[i]*sortArr[sortArr.length-i-1]
+      }
+  console.log(sum);
 }
